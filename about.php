dee8a38bbbeda7e9825aef2e7b25119d43494dbb
mc1mc <42059891+mc1mc@users.noreply.github.com>
2018-08-26 04:26:05 -0700

    Initial commit

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<link rel ="stylesheet" type="text/css" href="montanaSugars.css">
+    <head>
+	<h1>Montana Sugars&emsp;</h1>
+                <form action="index.php">
+                    <input type="submit" value="Home"/>
+                </form>
+                <form action="pricing.php">
+                    <input type="submit" value="Pricing"/>
+                </form>
+                <form action="about.php">
+                    <input type="submit" value="About"/>
+                </form><br>
+        <h2>
+            Always delivering quality product, always on time
+        </h2>
+    </head>
+
+    
+</html>
+
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--Author: Met Afework
+	Date: 8/25/18
+	File:	  TheSite.html
+	Purpose:  HTML Practice
+-->
+
+<html>
+<head>
+	<h1>Montana Sugars&emsp;</h1>
+                <form action="index.php">
+                    <input type="submit" value="Home"/>
+                </form>
+                <form action="pricing.php">
+                    <input type="submit" value="Pricing"/>
+                </form>
+                <form action="about.php">
+                    <input type="submit" value="About"/>
+                </form>
+    <link rel ="stylesheet" type="text/css" href="montanaSugars.css">
+</head>
+
+<body>
+	
+            
+            
+	<!--<h1 style="background-color:"Red;">Tony's Sugars</h1>-->
+        <br>
+
+        <h2>
+                Always delivering quality product, always on time
+        </h2>
+        <h3>
+            Welcome to Montana Sugar Company, home of the world's most addictive sugar
+        </h3>
+        <div>
+            <p>
+                &emsp;&emsp;Welcome to Tony's Sugars. Bringing you the finest sugars Colombia has to offer since
+                1983. In assciation with the Sosa group, our company prides itself on providing 70% of America's
+                sugar supplies. We don't just want to have our customers love our fine product we want our
+                customers to accept nothing else.
+            <p>
+        </div>
+        
+
+</body>
+<footer>
+    Copyright 2018 Montana Sugar Company. All rights reserved.
+</footer>
+</html>
